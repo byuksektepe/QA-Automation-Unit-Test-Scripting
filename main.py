9fccaf08f400456815b5b697e0c25d456484a9f2
Berkantyuks <byuksektepe@hotmail.com>
2022-06-01 18:14:28 +0300

    First Commit

---
+++ main.py
@@ -0,0 +1,11 @@
+import requests
+
+def api():
+
+    url = "https://jsonplaceholder.typicode.com/photos"
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    api()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
