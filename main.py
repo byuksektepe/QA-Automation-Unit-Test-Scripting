f57e48e873c8157183b3def6aa072121ec15c8ff
Berkantyuks <byuksektepe@hotmail.com>
2022-06-01 18:25:08 +0300

    init and test REST API

--- main.py
+++ main.py
@@ -1,19 +1,26 @@
 import requests
 
-def rest_api():
+class main:
 
     url = "https://jsonplaceholder.typicode.com/photos"
-    response_get = requests.get(url)
 
-    # print(response.json())
-    jsonPayload = {'albumId':1, 'title':'test',
-                   'url':'thisisatesturl.com', 'thumbnailUrl':'thisisatesturl.com'}
+    def rest_api_get():
 
-    response_post = requests.post(url, json=jsonPayload)
-    response_post.json()
+
+        response = requests.get(main.url)
+
+        print(response.json())
+
+
+    def rest_api_post():
+        jsonPayload = {'albumId':1, 'title':'test',
+                       'url':'thisisatesturl.com', 'thumbnailUrl':'thisisatesturl.com'}
+
+        response = requests.post(main.url, json=jsonPayload)
+        print(response.json())
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    rest_api()
+    main.rest_api_post()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
