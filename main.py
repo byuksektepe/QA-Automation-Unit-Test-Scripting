17d3897aec9d01228cfc62b942712ca00ecfc553
Berkantyuks <byuksektepe@hotmail.com>
2022-06-01 18:16:01 +0300

    init and test REST API

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ import requests
 def api():
 
     url = "https://jsonplaceholder.typicode.com/photos"
+    response = requests.get(url)
+
+    print(response)
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
