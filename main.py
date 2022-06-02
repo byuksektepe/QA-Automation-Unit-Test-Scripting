52190f2895007a5a4ce2865e576d6779416a80cd
Berkantyuks <byuksektepe@hotmail.com>
2022-06-02 22:51:13 +0300

    google sheets api updated

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import time
 import requests
 from tests.read_in_data import read_in_data_test_timing_data, read_in_data_test_analysis_data
 from tests.google_chart_api import google_chart_api_test_timing_data, google_chart_api_test_analysis_data
+from tests.google_sheets_api import google_sheets_api_runtime_test
 
 class main:
 
@@ -61,10 +62,13 @@ class main:
 
 
 if __name__ == '__main__':
+
     read_in_data_test_timing_data = read_in_data_test_timing_data().start()
     google_chart_api_test_timing_data = google_chart_api_test_timing_data().start()
 
     read_in_data_test_analysis_data = read_in_data_test_analysis_data().start()
     google_chart_api_test_analysis_data = google_chart_api_test_analysis_data().start()
+
+    # google_sheets_api_runtime_test = google_sheets_api_runtime_test().start()
 
 
--- tests/google_sheets_api.py
+++ tests/google_sheets_api.py
@@ -1,5 +1,6 @@
 import csv
 import gspread
+from datetime import date
 from google.oauth2.service_account import Credentials
 from tests.google_chart_api import global_variables as chart_gv
 
@@ -8,9 +9,8 @@ class google_sheets_api_runtime_test:
 
     def start(self):
 
-        scope = ['https://spreadsheets.google.com/feeds',
-                 'https://www.googleapis.com/auth/drive']
-        creds = Credentials.from_json_keyfile_name('keys/qatestscript-f08c8552be9f.json', scope)
+        scope = ['https://spreadsheets.google.com/feeds']
+        creds = Credentials.from_service_account_file('tests/keys/qatestscript-f08c8552be9f.json', scope)
         client = gspread.authorize(creds)
 
         sheet = client.open('QATestData').sheet1
@@ -32,6 +32,41 @@ class google_sheets_api_runtime_test:
             file_read = csv.reader(csv_file)
             for row in file_read:
                 csv_data.append(row)
+        run_date = csv_data[1][2]
+
+        run_date = csv_data[1][2]
+        spreadsheet_header_row = run_times[0]
+        spreadsheet_header_row.append(run_date)
+        del spreadsheet_header_row[0]
+        # add the data from each run to the end of the same row in the spreadsheet data
+        for spreadsheet_row, csv_row in zip(run_times[1:], csv_data[1:]):
+            new_value = csv_row[1]  # second column in the csv data is the new run time
+            spreadsheet_row.append(new_value)
+            del spreadsheet_row[0]
+
+        # write the new spreadsheet data back into the spreadsheet
+        for row_index, row in enumerate(run_times):
+            for col_index, cell in enumerate(row):
+                sheet.update_cell(row_index + 1, col_index + 3, cell)
+
+        # create a chart
+
+        avg_data = sheet.col_values(2)
+
+        chart_data = [["Test Name", "Diff From Avg"]]
+        for avg, current in zip(avg_data[1:], csv_data[1:]):
+            diff = float(avg) - float(current[1])
+            chart_data.append([current[0], diff])
+
+        chart_data_str = ''
+        for row in chart_data[1:]:
+            chart_data_str += '%s,\n' % row
+
+        completed_html = chart_gv.html_string.substitute(labels=chart_data[0], data=chart_data_str)
+
+        file_date = date.today()
+        with open('tests/test_docs/charts/runtime_sheets_chart_' + str(file_date) + '.html', 'w') as f:
+            f.write(completed_html)
 
     pass
 
