f6d83a6db4975c7075ddb6810a8a7b063ebdcafd
Berkantyuks <byuksektepe@hotmail.com>
2022-06-03 16:25:42 +0300

    Analyze jira data updated

--- tests/analyze_jira_data.py
+++ tests/analyze_jira_data.py
@@ -1,6 +1,8 @@
 import requests
+import json
 
 class rest_api:
+
     def start(self):
 
         url = "http://jira.<company name>/rest/api/latest/search?jql=reporter=dwesterveld"
@@ -8,3 +10,14 @@ class rest_api:
 
         return response
     pass
+
+class analyze_jira_data_test:
+
+    def start(self):
+
+        json_data = open('tests/test_docs/JiraData.json')
+        data = json.loads(json_data)
+
+        status_counts = {}
+        for project in data['projects']:
+            for issue in project['issues']:
